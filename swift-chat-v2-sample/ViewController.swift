4ec3f2e511b9d9b4df245075042005ad35567d59
Miranda Strand <miranda.strand@kustomer.com>
2021-08-13 14:29:20 -0400

    Revert "Login flow"
    This reverts commit da5788bcdd3bfe683195b6ef91959936bbc707b3.

---
+++ swift-chat-v2-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-chat-v2-sample
+//
+//  Created by Miranda Strand on 8/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
