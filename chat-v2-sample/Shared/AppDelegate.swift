4b4d3b74c9fcafdb156c6aca4d1972badf1a282c
Raymond Jones <raymond.jones@kustomer.com>
2024-02-05 15:37:02 -0500

    add push to app delegate

--- chat-v2-sample/Shared/AppDelegate.swift
+++ chat-v2-sample/Shared/AppDelegate.swift
@@ -10,21 +10,30 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Configure Kustomer with your API Key
+    // Make sure to do this in your app delegate!
+    let options = KustomerOptions()
+    options.logLevels = [.debug, .error]
+    _ = Kustomer.configure(apiKey: Constants.apiKey, options: options, launchOptions: launchOptions)
+    return true
+  }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Configure Kustomer with your API Key
-        // Make sure to do this in your app delegate!
-        let options = KustomerOptions()
-        options.logLevels = [.debug, .error]
-        _ = Kustomer.configure(apiKey: Constants.apiKey, options: options, launchOptions: launchOptions)
-        return true
-    }
+  // MARK: UISceneSession Lifecycle
 
-    // MARK: UISceneSession Lifecycle
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-    
+  // MARK: - Push
+
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Kustomer.didRegisterForRemoteNotifications(deviceToken: deviceToken)
+  }
+
+  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    Kustomer.didFailToRegisterForRemoteNotifications(error: error)
+  }
 }
 
